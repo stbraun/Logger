54d8284546b248912ff919189d1e8abd42582b65
sb <sb@action.ms>
2018-03-06 19:17:38 +0100

    Initial Commit

---
+++ Logger/Logger.h
@@ -0,0 +1,19 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by Stefan Braun on 06.03.18.
+//  Copyright © 2018 Stefan Braun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
